5d8fccc65f4693bcec0798e0932b85a62c87303f
Daniele Di Eleuterio <89977277+DanieleBool@users.noreply.github.com>
2023-11-22 22:01:36 +0100

    progetto pronto

--- src/app/message-form/message-form.component.ts
+++ src/app/message-form/message-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-message-form',
@@ -7,54 +8,54 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./message-form.component.css']
 })
 export class MessageFormComponent {
+  private apiHost = 'https://api.voxelstudios.it/';
+
+  name: string = '';
+  email: string = '';
+  message: string = '';
   isCheckboxChecked: boolean = false;
   isSubmitted: boolean = false;
   showError: boolean = false;
 
-  // Aggiungi le proprietà per raccogliere i dati del form
-  name: string = '';
-  email: string = '';
-  message: string = '';
-
-  // Inietta HttpClient nel costruttore
   constructor(private http: HttpClient) {}
 
-  onSubmit(button: HTMLButtonElement) {
+  onSubmit(button: HTMLButtonElement) { 
     if (this.isCheckboxChecked) {
       this.isSubmitted = true;
       this.showError = false;
-  
-      // Prepara i dati da inviare
-      const formData = {
-        name: this.name,
-        email: this.email,
-        message: this.message
-      };
-  
-      // Invia i dati al tuo endpoint API Gateway
-      this.http.post('https://hsey0ho24k.execute-api.eu-north-1.amazonaws.com/fase1', formData).subscribe({
-        next: (response) => {
-          console.log('Email inviata con successo', response);
-          // Resetta il form qui se necessario
-        },
-        error: (error) => {
-          console.error('Errore nell\'invio dell\'email', error);
-        },
-        complete: () => console.log('Richiesta completata') // Opzionale
-      });
-  
-      // Gestione dell'animazione del pulsante
       button.classList.add('submit-button-animate');
+      this.sendContactMessage(this.email, this.message).then(() => {
+        // Gestisci la risposta qui
+      }).catch(err => {
+        // Gestisci l'errore qui
+      });
       setTimeout(() => {
         button.classList.remove('submit-button-animate');
-      }, 1000);
-  
-      setTimeout(() => {
         this.isSubmitted = false;
       }, 3500);
     } else {
       this.showError = true;
     }
   }
-  
+
+  public sendContactMessage(email: string, message: string): Promise<any> {
+    const url = this.apiHost + 'contact/sendMessage';
+    return this.postJson(url, { email, message });
+  }
+
+  private postJson(url: string, json: Object, addHeaders: Array<{ name: string, value: string }> = []): Promise<any> {
+    let data = JSON.stringify(json);
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    for (let header of addHeaders) {
+      headers = headers.append(header.name, header.value);
+    }
+    return this.http.post(url, data, { headers })
+      .toPromise()
+      .catch(err => {
+        console.error(err);
+        throw err;  // Rilancia l'errore per gestirlo più in alto
+      });
+  }
 }
