b251878d68af99be1a5e1bebc5474a1115d9a94e
AresHelios <marcopatrignani.mp@gmail.com>
2023-11-23 23:50:56 +0100

    push italiano

--- src/app/message-form/message-form.component.ts
+++ src/app/message-form/message-form.component.ts
@@ -10,24 +10,30 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class MessageFormComponent {
   private apiHost = 'https://api.voxelstudios.it/';
 
-  name: string = '';
-  email: string = '';
-  message: string = '';
+  // Get values from the form using IDs
+  name: string = document.getElementById('name') ? document.getElementById('name')!.innerText : "";
+  email: string = document.getElementById('email') ? document.getElementById('email')!.innerText : "";
+  message: string = document.getElementById('message') ? document.getElementById('message')!.innerText : "";
   isCheckboxChecked: boolean = false;
   isSubmitted: boolean = false;
   showError: boolean = false;
 
   constructor(private http: HttpClient) {}
 
-  onSubmit(button: HTMLButtonElement) { 
+  onSubmit(button: HTMLButtonElement) {
+  console.log('submit');
     if (this.isCheckboxChecked) {
+        console.log('Messaggio inviato col cazz');
       this.isSubmitted = true;
       this.showError = false;
       button.classList.add('submit-button-animate');
-      this.sendContactMessage(this.email, this.message).then(() => {
+      this.sendContactMessage(this.name, this.email, this.message).then(() => {
         // Gestisci la risposta qui
+        console.log(this.name, this.email, this.message);
+        console.log('Messaggio inviato con successo');
       }).catch(err => {
         // Gestisci l'errore qui
+        console.error(err);
       });
       setTimeout(() => {
         button.classList.remove('submit-button-animate');
@@ -35,12 +41,16 @@ export class MessageFormComponent {
       }, 3500);
     } else {
       this.showError = true;
+      console.log('Checkbox non selezionata');
     }
   }
 
-  public sendContactMessage(email: string, message: string): Promise<any> {
+  public sendContactMessage(name: string, email: string, message: string): Promise<any> {
+    console.log(name, email, message);
+    console.log('Messaggio inviato con successo');
     const url = this.apiHost + 'contact/sendMessage';
-    return this.postJson(url, { email, message });
+    //print the values
+    return this.postJson(url, { name, email, message });
   }
 
   private postJson(url: string, json: Object, addHeaders: Array<{ name: string, value: string }> = []): Promise<any> {
@@ -59,3 +69,4 @@ export class MessageFormComponent {
       });
   }
 }
+
